7195a00b4a0b176b8f58a155fcbcac7ebad0944d
ethluz <lvxing67@126.com>
2018-10-04 11:11:35 +0800

    日志中间件：设置日志目录，按日期分割成文件

--- middleware/log/logger.js
+++ middleware/log/logger.js
@@ -5,16 +5,30 @@ const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 
 module.exports = ( options ) => {
   const contextLogger = {}  
+  
+  //按日期打印日志
+  const config = {
+    appenders: {
+        cheese: {
+         type: 'dateFile', // 日志类型 
+         filename: `logs/task`,  // 输出的文件名
+         pattern: '-yyyy-MM-dd.log',  // 文件名增加后缀
+         alwaysIncludePattern: true   // 是否总是有后缀名
+       }
+    },
+    categories: {
+      default: {
+        appenders: ['cheese'],
+        level:'info'
+      }
+    }
+  } 
 
+  const logger = log4js.getLogger('cheese');
   return async (ctx, next) => {
     const start = Date.now()
-    log4js.configure({
-      appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-      categories: { default: { appenders: ['cheese'], level: 'info' } }
-    }); 
-    const logger = log4js.getLogger('cheese');
-
-
+    log4js.configure(config)
+   
       // 循环methods将所有方法挂载到ctx 上
       methods.forEach((method, i) => {
         contextLogger[method] = (message) => {
