0204d238ebbead351db2c9bb92a2adb247953fb1
ethluz <lvxing67@126.com>
2018-10-09 15:12:18 +0800

    灵活的日志配置及添加客户端信息到日志

---
+++ middleware/log/access.js
@@ -0,0 +1,22 @@
+// 丰富日志信息
+// 在 ctx 对象中，有一些客户端信息是我们数据统计及排查问题所需要的，所以完全可以利用这些信息来丰富日志内容。在这里，我们只需要修改挂载 ctx 对象的 log 函数的传入参数：
+// logger[method](message)
+// 参数 message 是一个字符串，所以我们封装一个函数，用来把信息与上下文 ctx 中的客户端信息相结合，并返回字符串。
+// 增加日志信息的封装文件 mi-log/access.js：
+module.exports = (ctx, message, commonInfo) => {
+    const {
+      method,  // 请求方法 get post或其他
+      url,		  // 请求链接
+      host,	  // 发送请求的客户端的host
+      headers	  // 请求中的headers
+    } = ctx.request;
+    const client = {
+      method,
+      url,
+      host,
+      message,
+      referer: headers['referer'],  // 请求的源地址
+      userAgent: headers['user-agent']  // 客户端信息 设备及浏览器信息
+    }
+    return JSON.stringify(Object.assign(commonInfo, client));
+  }
--- middleware/log/logger.js
+++ middleware/log/logger.js
@@ -1,44 +1,72 @@
 // const log4js = require('log4js');
 import log4js from 'log4js'
+// 引入日志输出信息的封装文件
+// const access = require("./access.js");
+import access from './access'
 
 const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 
-module.exports = ( options ) => {
-  const contextLogger = {}  
-  
-  //按日期打印日志
-  const config = {
-    appenders: {
-        cheese: {
-         type: 'dateFile', // 日志类型 
-         filename: `logs/task`,  // 输出的文件名
-         pattern: '-yyyy-MM-dd.log',  // 文件名增加后缀
-         alwaysIncludePattern: true   // 是否总是有后缀名
-       }
-    },
-    categories: {
-      default: {
-        appenders: ['cheese'],
-        level:'info'
-      }
-    }
-  } 
 
-  const logger = log4js.getLogger('cheese');
-  return async (ctx, next) => {
-    const start = Date.now()
-    log4js.configure(config)
-   
-      // 循环methods将所有方法挂载到ctx 上
-      methods.forEach((method, i) => {
-        contextLogger[method] = (message) => {
-          logger[method](message)
+// 提取默认公用参数对象
+const baseInfo = {
+    appLogLevel: 'debug',  // 指定记录的日志级别
+    dir: 'logs',		// 指定日志存放的目录名
+    env: 'dev',   // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
+    projectName: 'koa2-tutorial',  // 项目名，记录在日志中的项目信息
+    serverIp: '0.0.0.0'		// 默认情况下服务器 ip 地址
+}
+
+const { env, appLogLevel, dir, serverIp, projectName } = baseInfo
+// 增加常量，用来存储公用的日志信息
+const commonInfo = { projectName, serverIp }
+module.exports = (options) => {
+    const contextLogger = {}
+    const appenders = {}
+
+    appenders.cheese = {
+        type: 'dateFile',
+        filename: `${dir}/task`,
+        pattern: '-yyyy-MM-dd.log',
+        alwaysIncludePattern: true
+    }
+    // 环境变量为dev local development 认为是开发环境
+    if (env === "dev" || env === "local" || env === "development") {
+        appenders.out = {
+            type: "console"
         }
-     })
-    ctx.log = contextLogger;
-    await next()
-    const end = Date.now()
-    const responseTime = end - start;
-    logger.info(`响应时间为${responseTime/1000}s`);
-  }
+    }
+    let config = {
+        appenders,
+        categories: {
+            default: {
+                appenders: Object.keys(appenders),
+                level: appLogLevel
+            }
+        }
+    }
+
+    const logger = log4js.getLogger('cheese');
+    return async (ctx, next) => {
+        const start = Date.now()
+        log4js.configure(config)
+
+        // 循环methods将所有方法挂载到ctx 上
+        methods.forEach((method, i) => {
+
+            contextLogger[method] = (message) => {
+                // logger[method](message)
+                // 将入参换为函数返回的字符串
+                logger[method](access(ctx, message, commonInfo))
+            }
+        })
+
+        ctx.log = contextLogger;
+        await next()
+        const end = Date.now()
+        const responseTime = end - start;
+        // logger.info(`响应时间为${responseTime / 1000}s`);
+        logger.info(access(ctx, {
+            responseTime: `响应时间为${responseTime/1000}s`
+          }, commonInfo))
+    }
 }
