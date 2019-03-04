baf775872abf79a12e1b80f794f647229e8e579b
ethluz <lvxing67@126.com>
2019-03-04 21:27:57 +0800

    兼容端口自定义

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("babel-core/register")({
     presets: ['env', 'stage-0']
 });
 require("babel-polyfill");   //引入这个文件babel-polyfill很重要，否则出现错误
-
+const PORT = process.env.PORT || 3000
 const Koa = require('koa')
 const app = new Koa()
 const router = require('./router');
@@ -14,6 +14,11 @@ const middleware = require('./middleware')
 middleware(app)
 
 router(app);
-app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+// app.listen(3000, () => {
+//   console.log('server is running at http://localhost:3000')
+// })
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`)
 })
+  
